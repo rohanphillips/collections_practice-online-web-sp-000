f9b4521cb5d9bbee7749b6eb37d339daa768df14
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 15:37:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,5 +52,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-    array.each_with_index.collect{|element, index| }
+    array.each_with_index.collect{|element, index| (index != 1 ? element + "s" : element)}
 end
