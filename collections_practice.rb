4351eae036cf4e1f8868e5e568e985e19578b848
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 15:05:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,9 @@ end
 def sort_array_char_count(array)
   array.sort{|x,y| x.length <=> y.length}
 end
+def swap_elements(array, x, y)
+  myx = array[x]
+  myy = array[y]
+  array[y] = myx
+  array[x] = myy
+end
